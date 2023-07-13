7ac6b22384a0abdc3c3396b6e26f390fd70f3e0c
Abdela Umer <adheremo65@gmail.com>
2023-07-13 19:33:18 -0400

    list_compernshions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    x = []
+    for n in num_list:
+        if n%2 ==0:
+            x.append(n)
+    return x
+    
 
 def make_exclamation(sentence_list):
+    y = []
+    for i in sentence_list:
+        y.append(i + "!")
+    return y
+
+
     pass
